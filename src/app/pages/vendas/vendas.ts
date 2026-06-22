01104073973f684861fea74d247d559003ef98bf
Carlos E. Araújo <110763014+carlos-e-araujo@users.noreply.github.com>
2026-06-21 23:44:30 -0300

    Add components for main application pages

---
+++ src/app/pages/vendas/vendas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendas',
+  imports: [],
+  templateUrl: './vendas.html',
+  styleUrl: './vendas.css',
+})
+export class Vendas {
+
+}
