eda97d0d5eebc15b82a32d1a77d22c74a7767834
Carlos E. Araújo <110763014+carlos-e-araujo@users.noreply.github.com>
2026-06-21 23:28:50 -0300

    Add sidebar component and update main layout with logo and welcome message

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { Sidebar } from './shared/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
+  imports: [Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
