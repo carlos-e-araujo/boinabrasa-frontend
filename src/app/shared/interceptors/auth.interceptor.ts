51b317915dc818ab5861da16fd7514ac703de08a
Carlos E. Araújo <110763014+carlos-e-araujo@users.noreply.github.com>
2026-07-02 12:37:29 -0300

    feat: implement error handling in auth interceptor to redirect to login on 401 and 403 status codes

--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -1,10 +1,15 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
+
+  const router = inject(Router);
 
   if (token) {
     req = req.clone({
@@ -14,5 +19,13 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 || error.status === 403) {
+        localStorage.removeItem('token');
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    })
+  );
 };
